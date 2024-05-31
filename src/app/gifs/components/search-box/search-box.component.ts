d860193b41f5be70e4ad5cd98168645cd9009c32
Juan Gomez <gomezsandovalpablo@gmail.com>
2024-05-30 17:05:46 -0700

    GifsService Created

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
   <h5>Buscar</h5>
-  <input type="text" class="form-control" placeholder="Buscar GIFs...">
+  <input
+    (keyup.enter)="searchTag()"
+    #txtTagInput
+    type="text"
+    class="form-control"
+    placeholder="Buscar GIFs...">
   `,
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService ) {}
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+
+  }
 }
